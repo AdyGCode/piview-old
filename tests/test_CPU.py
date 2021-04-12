2d6860e33aa245f261a57c8647b7b702e333d11b
Sander Huijsen <sander.huijsen@gmail.com>
2021-04-12 19:53:29 +0800

    Updated the tests to use the static methods.

--- tests/test_CPU.py
+++ tests/test_CPU.py
@@ -2,20 +2,17 @@ from PiView_AG.CPU import CPU
 
 
 def test_speed():
-    cpu = CPU()
-    results = cpu.speed()
+    results = CPU.speed()
     assert 0.0 <= results <= 100.0
 
 
 def test_max_load():
-    cpu = CPU()
-    results = cpu.max_load()
+    results = CPU.max_load()
     expected = "50.0"
     assert 0.0 <= results <= 100.0
 
 
 def test_temperature():
-    cpu = CPU()
-    results = cpu.temperature()
+    results = CPU.temperature()
     expected = "0.00"
     assert results == expected
--- tests/test_Utils_line_draw.py
+++ tests/test_Utils_line_draw.py
@@ -2,49 +2,42 @@ from PiView_AG.Utils import Utils
 
 
 def test_draw_no_line():
-    utils = Utils()
-    results = utils.draw_line(length=0)
+    results = Utils.draw_line(length=0)
     expected = ""
     assert results == expected
 
 
 def test_draw_no_characters():
-    utils = Utils()
-    results = utils.draw_line(characters="1234567890", length=0)
+    results = Utils.draw_line(characters="1234567890", length=0)
     expected = ""
     assert results == expected
 
 
 def test_draw_line_ten_spaces():
-    utils = Utils()
-    results = utils.draw_line(characters=" ", length=10)
+    results = Utils.draw_line(characters=" ", length=10)
     expected = "          "
     assert results == expected
 
 
 def test_draw_line_ten_dashes():
-    utils = Utils()
-    results = utils.draw_line(characters="-", length=10)
+    results = Utils.draw_line(characters="-", length=10)
     expected = "----------"
     assert results == expected
 
 
 def test_draw_line_eleven_dash_dots():
-    utils = Utils()
-    results = utils.draw_line(characters="-.", length=11)
+    results = Utils.draw_line(characters="-.", length=11)
     expected = "-.-.-.-.-.-"
     assert results == expected
 
 
 def test_draw_line_two_characters_from_six():
-    utils = Utils()
-    results = utils.draw_line(characters="123456", length=2)
+    results = Utils.draw_line(characters="123456", length=2)
     expected = "12"
     assert results == expected
 
 
 def test_draw_default_line():
-    utils = Utils()
-    results = utils.draw_line()
+    results = Utils.draw_line()
     expected = "-=--=--=--=--=--=--=--=--=--=--=--=--=--"
     assert results == expected
